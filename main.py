4ba3220ff7555ec3b58891231cec92d55de9ea1b
Eren <57950444+adenzu@users.noreply.github.com>
2024-06-21 15:00:12 +0300

    added foldered outputs

--- main.py
+++ main.py
@@ -362,12 +362,13 @@ class ExtractionThread(QThread):
     progress_update = pyqtSignal(str)
     process_finished = pyqtSignal()
 
-    def __init__(self, input_dir: str, output_dir: str, split_joint_panels: bool = False, fallback: bool = True):
+    def __init__(self, input_dir: str, output_dir: str, split_joint_panels: bool = False, fallback: bool = False, output_to_folders: bool = False):
         super().__init__()
         self.input_dir = input_dir
         self.output_dir = output_dir
         self.split_joint_panels = split_joint_panels
         self.fallback = fallback
+        self.output_to_folders = output_to_folders
 
     def run(self):
         files = os.listdir(self.input_dir)
@@ -377,9 +378,17 @@ class ExtractionThread(QThread):
                 return
             self.progress_update.emit(f"Processing file {i+1}/{total_files}")
             image_name, image_ext = os.path.splitext(image.image_name)
-            for k, panel in enumerate(generate_panel_blocks(image.image, split_joint_panels=self.split_joint_panels, fallback=self.fallback)):
-                out_path = os.path.join(self.output_dir, f"{image_name}_{k}{image_ext}")
-                cv2.imwrite(out_path, panel)
+            if self.output_to_folders:
+                output_folder = os.path.join(self.output_dir, image_name)
+                if not os.path.exists(output_folder):
+                    os.makedirs(output_folder)
+                for k, panel in enumerate(generate_panel_blocks(image.image, split_joint_panels=self.split_joint_panels, fallback=self.fallback)):
+                    out_path = os.path.join(output_folder, f"{image_name}_{k}{image_ext}")
+                    cv2.imwrite(out_path, panel)
+            else:
+                for k, panel in enumerate(generate_panel_blocks(image.image, split_joint_panels=self.split_joint_panels, fallback=self.fallback)):
+                    out_path = os.path.join(self.output_dir, f"{image_name}_{k}{image_ext}")
+                    cv2.imwrite(out_path, panel)
         self.process_finished.emit()
 
 # TODO: Clean up the GUI positioning and resizing code
@@ -482,7 +491,14 @@ class MainWindow(QMainWindow):
             self.cancelButton.setEnabled(True)
 
             # Start extraction thread
-            self.extraction_thread = ExtractionThread(input_dir, output_dir, self.split_joint_panels_checkbox.isChecked(), self.fallback_checkbox.isChecked())
+            self.extraction_thread = ExtractionThread(
+                input_dir, 
+                output_dir, 
+                self.splitJointPanelsCheckbox.isChecked(), 
+                self.fallbackCheckbox.isChecked(), 
+                self.outputToFoldersCheckbox.isChecked()
+            )
+            
             self.extraction_thread.progress_update.connect(self.update_progress)
             self.extraction_thread.process_finished.connect(self.extracting_finished)
             self.extraction_thread.start()
